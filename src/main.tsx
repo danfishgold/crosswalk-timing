d4de40c63082e355898120ac0c58bd0c8e3b4d1a
Dan Fishgold <d.fishgold@gmail.com>
2022-05-24 23:24:22 +0300

    Revert to React 17 because recharts doesn't like 18 :(

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './App'
 import './index.css'
 import { store } from './store'
 
-const container = document.getElementById('root')
-const root = createRoot(container!)
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
       <Provider store={store}>
@@ -17,4 +14,5 @@ root.render(
       </Provider>
     </ChakraProvider>
   </React.StrictMode>,
+  document.getElementById('root'),
 )
