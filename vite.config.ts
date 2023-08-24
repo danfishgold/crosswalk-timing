b60bb092f6922274663eb109ac6652bee01a9990
Dan Fishgold <d.fishgold@gmail.com>
2023-08-24 20:27:53 +0300

    Add tests for state coding

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
@@ -8,4 +10,8 @@ export default defineConfig({
       jsxImportSource: '@emotion/react',
     }),
   ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 })
